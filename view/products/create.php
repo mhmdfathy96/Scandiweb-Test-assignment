748049ef9527c64c706f0e7e5eed6de4c08a5c2b
mhmdfathy96 <mhmdfathy96@gmail.com>
2022-03-22 13:43:12 +0200

    last

--- view/products/create.php
+++ view/products/create.php
@@ -1,4 +1,4 @@
-<form method="POST">
+<form method="POST" id="product_form">
     <div class="page-header">
         <p class="header-title">Create new Product</p>
         <div class="header-buttons">
@@ -16,54 +16,54 @@
     <?php endif; ?>
     <div class="input-style">
         <label>SKU</label>
-        <input type="text" class="form-control" name="SKU" value="<?php echo $productData['SKU'] ?>">
+        <input type="text" class="form-control" id="sku" name="SKU" value="<?php echo $productData['SKU'] ?>">
     </div>
     <div class="input-style">
         <label>Name</label>
-        <input type="text" class="form-control" name="Name" value="<?php echo $productData['Name'] ?>">
+        <input type="text" class="form-control" id="name" name="Name" value="<?php echo $productData['Name'] ?>">
     </div>
     <div class="input-style">
         <label>Price</label>
-        <input type="number" step=".01" class="form-control" name="Price" value="<?php echo $productData['Price'] ?>">
+        <input type="number" step=".01" class="form-control" id="price" name="Price" value="<?php echo $productData['Price'] ?>">
     </div>
-    <select class="dropdown" id="type-selector" name="type">
+    <select class="dropdown" id="productType" name="type">
         <option class="dropdown-item" value="" id="default" <?php if(($_POST['type']??'')==''):?> selected <?php endif ?> >Please Select a type</option>
-        <option class="dropdown-item" value="DVD-disc" id="DVD-disc"<?php if(($_POST['type']??'')=='DVD-disc'):?> selected <?php endif ?> >DVD-disc</option>
+        <option class="dropdown-item" value="DVD" id="DVD"<?php if(($_POST['type']??'')=='DVD'):?> selected <?php endif ?> >DVD</option>
         <option class="dropdown-item" value="Book" id="Book"<?php if(($_POST['type']??'')=='Book'):?> selected <?php endif ?> >Book</option>
         <option class="dropdown-item" value="Furniture" id="Furniture"<?php if(($_POST['type']??'')=='Furniture'):?> selected <?php endif ?> >Furniture</option>
     </select>
     <div class="input-style" id="inputSize" style="display: none">
         <label>Size (in MB)</label>
-        <input type="number" class="form-control" name="Size" value="<?php echo $productData['Size'] ?>">
+        <input type="number" class="form-control" name="Size" id="size" value="<?php echo $productData['Size'] ?>">
     </div>
     <div class="input-style" id="inputWeight" style="display: none">
         <label>Weight (in KG)</label>
-        <input type="number" step=".01" class="form-control" name="Weight" value="<?php echo $productData['Weight'] ?>">
+        <input type="number" step=".01" class="form-control" id="weight" name="Weight" value="<?php echo $productData['Weight'] ?>">
     </div>
     <div class="input-style" id="inputHeight" style="display: none">
         <label>Height (in M)</label>
-        <input type="number" step=".01" class="form-control" name="Height" value="<?php echo $productData['Height'] ?>">
+        <input type="number" step=".01" class="form-control" id="height" name="Height" value="<?php echo $productData['Height'] ?>">
     </div>
     <div class="input-style" id="inputWidth" style="display: none">
         <label>Width (in M)</label>
-        <input type="number" step=".01" class="form-control" name="Width" value="<?php echo $productData['Width'] ?>">
+        <input type="number" step=".01" class="form-control" id="width" name="Width" value="<?php echo $productData['Width'] ?>">
     </div>
     <div class="input-style" id="inputLength" style="display: none">
         <label>Length (in M)</label>
-        <input type="number" step=".01" class="form-control" name="Length" value="<?php echo $productData['Length'] ?>">
+        <input type="number" step=".01" class="form-control" id="length" name="Length" value="<?php echo $productData['Length'] ?>">
     </div>
 </form>
 <script>
     $(document).ready(function(){
-        $selectedElement=$("#type-selector").val();
+        $selectedElement=$("#productType").val();
         hideAllExcept($selectedElement);
     });
 
-    $("#type-selector").on("change",
+    $("#productType").on("change",
         function() {
             var type = $(this).val();
-            if (type == "DVD-disc") {
-                hideAllExcept('DVD-disc');
+            if (type == "DVD") {
+                hideAllExcept('DVD');
             } else if (type == "Book") {
                 hideAllExcept('Book');
             } else if (type == "Furniture") {
@@ -73,13 +73,13 @@
     );
 
     $("#saveButton").on("click",function(){
-        var selectedOption = $("#type-selector").val();
-        $("#type-selector").val(selectedOption).select();
+        var selectedOption = $("#productType").val();
+        $("#productType").val(selectedOption).select();
     });
 
     function hideAllExcept($element) {
         $allElements = {
-            'DVD-disc': ['inputSize'],
+            'DVD': ['inputSize'],
             'Book': ['inputWeight'],
             'Furniture': ['inputHeight', 'inputWidth', 'inputLength'],
         };
--- view/products/index.php
+++ view/products/index.php
@@ -1,46 +1,46 @@
-<?php
-
-use app\model\ProductModel;
-?>
-
-<form method="POST" action="/products/delete">
-    <div class="page-header">
-        <p class="header-title">Products List</p>
-        <div class="header-buttons">
-            <a href="/products/create" class="btn btn-success">Add</a>
-            <button type="submit" class="btn btn-sm btn-danger">Mass Delete</button>
-        </div>
-    </div>
-    <hr>
     <?php
-    if (empty($products)) : ?>
 
-        <div class="no-products">
-            <p> No Products Available Right Now , Please add some ..</p>
+    use app\model\ProductModel;
+    ?>
+
+    <form method="POST" action="/products/delete">
+        <div class="page-header">
+            <p class="header-title">Products List</p>
+            <div class="header-buttons">
+                <a href="/products/create" class="btn btn-success">ADD</a>
+                <button type="submit" class="btn btn-sm btn-danger">MASS DELETE</button>
+            </div>
         </div>
-    <?php elseif (!empty($products)) : ?>
-        <section class="products-list">
-            <?php
-            foreach ($products
-                as $product) :
-            ?>
-                <div>
-                    <input type="checkbox" class="card-checkbox" name="<?php echo $product->getSKU() ?>" value="<?php echo $product->getSKU() ?>">
-                    <p class="card-id"><?php echo 'SKU: '.$product->getSKU() ?></p>
-                    <p class="card-name"><?php echo 'Name: '.$product->getName() ?></p>
-                    <p class="card-price"><?php echo 'Price: '.$product->getPrice().' $' ?></p>
-                    <p class="card-specific"><?php
-                    if($product->getSize()!=0){
-                        echo 'Size: '.$product->getSize().' MB';
-                    }elseif($product->getWeight()!=0.0){
-                        echo 'Weight: '.$product->getWeight().' KG';
-                    }elseif($product->getHeight()!=0){
-                        echo 'Dimensions: '.$product->getHeight().'x'.$product->getWidth().'x'.$product->getLength();
-                    }
-                     ?></p>
-                    <p class="card-date"><?php echo 'Date: '.$product->getDate() ?></p>
-                </div>
-            <?php endforeach; ?>
-        <?php endif; ?>
-        </section>
-</form>
+        <hr>
+        <?php
+        if (empty($products)) : ?>
+
+            <div class="no-products">
+                <p> No Products Available Right Now , Please add some ..</p>
+            </div>
+        <?php elseif (!empty($products)) : ?>
+            <section class="products-list">
+                <?php
+                foreach ($products
+                    as $product) :
+                ?>
+                    <div>
+                        <input type="checkbox" class="delete-checkbox" name="<?php echo $product->getSKU() ?>" value="<?php echo $product->getSKU() ?>">
+                        <p class="card-id"><?php echo 'SKU: '.$product->getSKU() ?></p>
+                        <p class="card-name"><?php echo 'Name: '.$product->getName()?></p>
+                        <p class="card-price"><?php echo 'Price: '.$product->getPrice().' $' ?></p>
+                        <p class="card-specific"><?php
+                        if($product->getSize()!=0){
+                            echo 'Size: '.$product->getSize().' MB';
+                        }elseif($product->getWeight()!=0.0){
+                            echo 'Weight: '.$product->getWeight().' KG';
+                        }elseif($product->getHeight()!=0){
+                            echo 'Dimensions: '.$product->getHeight().'x'.$product->getWidth().'x'.$product->getLength();
+                        }
+                        ?></p>
+                        <p class="card-date"><?php echo 'Date: '.$product->getDate() ?></p>
+                    </div>
+                <?php endforeach; ?>
+            <?php endif; ?>
+            </section>
+    </form>
